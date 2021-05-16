9f355eee19ad31fc9c779e6ec3f23fd6ad63b6ff
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-05-16 16:53:30 +0200

    Rough try at being able to gather relaychain block number from tests

--- src/lib.rs
+++ src/lib.rs
@@ -219,6 +219,7 @@ pub mod pallet {
 			proof: MultiSignature,
 		) -> DispatchResultWithPostInfo {
 			ensure_signed(origin)?;
+			let _ = RelayChainBeacon::<T>::slot();
 			// Check the proof:
 			// 1. Is signed by an actual unassociated contributor
 			// 2. Signs a valid native identity
