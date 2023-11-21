ef7a7716b38487c84915c0ff62bd5c11c6f45d76
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-11-21 08:48:15 +0100

    fix compilation

--- src/mock.rs
+++ src/mock.rs
@@ -91,6 +91,7 @@ impl pallet_balances::Config for Test {
 	type FreezeIdentifier = ();
 	type MaxHolds = ();
 	type MaxFreezes = ();
+	type RuntimeFreezeReason = ();
 }
 
 pub struct MockedBlockProvider;
